91ec4c07d5545beb6d488397558caf129714da17
f0xG <122001660+f0xG@users.noreply.github.com>
2023-02-15 16:40:16 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import requests
+'''
+author: admin@xiaomi.com
+url: relay.xiaomi.com
+api.xiaomi.comhttps://github.com/f0xG/xiaomi-Admin
+xiaomi.srv
+'''
+appid = 'sadasdasdsad'
+appkey = 'asdmin2e3rsdas'
+accesskey = 'asdasdasdasdfdsf2e'
